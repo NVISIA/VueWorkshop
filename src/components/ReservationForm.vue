727955d1b6706cfaa7e3a5dd728c2dd3438e7eff
Joshua Armstrong <jarmstrong@nvisia.com>
2017-03-27 14:27:16 -0500

    Add form validation.

--- src/components/ReservationForm.vue
+++ src/components/ReservationForm.vue
@@ -9,36 +9,44 @@
                       </div>
                       <div class="large-6 columns">
                           <label>Number of Guests</label>
-                          <input type="number" name="guests" placeholder="#"/>
+                          <input type="number" v-model="guests" placeholder="#"
+                            min="1" inputmode="numeric" required />
                       </div>
                   </div>
 
                   <div class="row">
                       <div class="large-12 columns">
                           <label>Name:</label>
-                          <input type="text" name="name" placeholder="Enter your full name" :value="name" />
+                          <input type="text" v-model="name"
+                            placeholder="Enter your full name"
+                            minlength="1" autocapitalize="words"
+                            autocomplete="name" inputmode="verbatim" required />
                       </div>
                   </div>
                   <div class="row">
                       <div class="large-6 medium-6 columns">
                           <label>Phone:</label>
-                          <input type="text" name="phone" placeholder="###-###-####" />
+                          <input type="tel" v-model="phone"
+                            placeholder="###-###-####" autocomplete="tel"
+                            required
+                            pattern="^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$" />
                       </div>
                       <div class="large-6 medium-6 columns">
                           <label>Email:</label>
-                          <input type="text" name="email" placeholder="user@domain.ext" :value="email" />
+                          <input type="email" v-model="email"
+                            placeholder="user@domain.ext" autocomplete="email" />
                       </div>
                   </div>
                   <div class="row">
                       <div class="large-8 medium-8 columns">
                           <label>Seating:</label>
-                          <input type="radio" name="location" value="Red" id="noPreference"><label for="noPreference">No Preference</label>
-                          <input type="radio" name="location" value="Red" id="outside"><label for="outside">Outside</label>
-                          <input type="radio" name="location" value="Blue" id="inside"><label for="inside">Inside</label>
+                          <input type="radio" v-model="location" value="Red" id="noPreference"><label for="noPreference">No Preference</label>
+                          <input type="radio" v-model="location" value="Red" id="outside"><label for="outside">Outside</label>
+                          <input type="radio" v-model="location" value="Blue" id="inside"><label for="inside">Inside</label>
                       </div>
                       <div class="large-4 medium-4 columns">
                           <label>Other Info:</label>
-                          <input id="specialOccasion" name="isSpecialOccasion" type="checkbox"><label for="specialOccasion">Special Occasion</label>
+                          <input id="specialOccasion" v-model="isSpecialOccasion" type="checkbox"><label for="specialOccasion">Special Occasion</label>
                       </div>
                   </div>
                   <div class="row">
